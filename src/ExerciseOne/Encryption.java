0197400394a9d6e1c1edf72b78e88ef98288c07a
pd222dj <pd222dj@student.lnu.se>
2016-02-03 15:04:15 +0100

    Jag har lagt till en main metod...

--- src/ExerciseOne/Encryption.java
+++ src/ExerciseOne/Encryption.java
@@ -3,7 +3,10 @@ package ExerciseOne;
 /**
  * Created by Michael Johansson(mj223gn) on 2016-02-03.
  */
-public class Encryption {
-
-
+public class Encryption
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hej");
+    }
 }
